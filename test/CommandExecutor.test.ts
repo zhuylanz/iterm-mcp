f4bfd3ca08579faf429fa58369028fb96a9b397d
Ferris Lucas <ferris.lucas@gmail.com>
2025-03-28 23:19:15 -0500

    Use a control character in end to end test

--- test/CommandExecutor.test.ts
+++ test/CommandExecutor.test.ts
@@ -1,5 +1,6 @@
 import CommandExecutor from '../src/CommandExecutor.js';
 import TtyOutputReader from '../src/TtyOutputReader.js';
+import SendControlCharacter from '../src/SendControlCharacter.js';
 
 async function testExecuteCommand() {
   const executor = new CommandExecutor();
@@ -23,8 +24,29 @@ async function testExecuteCommand() {
   } catch (error) {
     console.error('Error executing command:', (error as Error).message);
   }
-
-  
 }
 
-testExecuteCommand();
+async function testSendControlCharacter() {
+  const executor = new CommandExecutor();
+  // Combine all arguments after the script name into a single command
+  const command = "sleep 120"
+  
+  try {
+    console.log("Executing sleep command...");
+    executor.executeCommand(command) // purposefully not awaited
+    
+    // give the command time to start
+    console.log("Waiting 5 seconds.");
+    await new Promise(resolve => setTimeout(resolve, 5000));
+
+    console.log("Sending control character...");
+    const ttyControl = new SendControlCharacter();
+    await ttyControl.send("c")
+
+    console.log("Control character sent.");
+  } catch (error) {
+    console.error('Error executing command:', (error as Error).message);
+  }
+}
+
+testSendControlCharacter()
